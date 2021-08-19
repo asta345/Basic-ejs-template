4201d9581795369c9c0996fa331b522430b23650
asta345 <81383112+asta345@users.noreply.github.com>
2021-08-20 04:49:48 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,115 +1,79 @@
-// const express=require('express');
-// //const { request } = require('http');
-// const path=require('path');
-// const  port=10000;
-// // to get all functionality of server we need to name it as app//
-// const app =express();
-// // to setup ejs//
-// app.set('view engine','ejs');
-// // to set a directory
-// app.set('views',path.join(__dirname,'views'));
-// var Contactlist=[
-// 	{
-// 		name:"asta",
-// 		phone:"9999955555"
-// 	},
-// 	{
-// 		name:"yami",
-// 		phone:"999999999"
-// 	},
-// 	{
-// 		name:"veldora",
-// 		phone:"99999999991"
-// 	},
-// 	{
-// 		name:"teach",
-// 		phone:"4445556667"
-// 	},
-// 	{      
-// 		name:"issei",
-// 		phone:"1234567891"
+const express=require('express');
+//const { request } = require('http');
+const path=require('path');
+const  port=10000;
+// to get all functionality of server we need to name it as app//
+const app =express();
+// to setup ejs//
+app.set('view engine','ejs');
+// to set a directory
+app.set('views',path.join(__dirname,'views'));
+app.use(express.urlencoded()); 
+var Contactlist=[
+	{
+		name:"asta",
+		phone:"9999955555"
+	},
+	{
+		name:"yami",
+		phone:"999999999"
+	},
+	{
+		name:"veldora",
+		phone:"99999999991"
+	},
+	{
+		name:"teach",
+		phone:"4445556667"
+	},
+	{      
+		name:"issei",
+		phone:"1234567891"
 
-// 	},
-// 	{
-//                name:"aokiji",
-// 	       phone:"345678912" 
-// 	},
+	},
+	{
+               name:"aokiji",
+	       phone:"345678912" 
+	},
 
-// ]
-// // we have given get request to fetch data//   
-// app.get('/',function(req, res){
-// 	console.log(__dirname ); //to display dirname in console
+]
+// we have given get request to fetch data//   
+app.get('/',function(req, res){
+	console.log(__dirname ); //to display dirname in console
 
-// 	// res.send('<h1>its is running</h1>');
-// 	return res.render('home',{title:"My Contacts List",
-// 	Contact_list:Contactlist});//here we are endering a view,declaring locals 
-// })
+	// res.send('<h1>its is running</h1>');
+	return res.render('home',{title:"My Contacts List",
+	Contact_list:Contactlist});//here we are endering a view,declaring locals 
+})
 
-// app.get('/practice',function(req,res){
-// 	console.log(__dirname);
+app.get('/practice',function(req,res){
+	console.log(__dirname);
 
-// 	return res.render('practice',{title:"LET so go!"});
-// });
-// //get requesting for home page where we written the form//
-// app.post('/create-contact', function(req, res){
-// 	return res.redirect('/home');
-//     });
+	return res.render('practice',{title:"LET so go!"});
+});
+//get requesting for home page where we written the form//
+app.post('/create-contact', function(req, res){
+	// Contactlist.push({
+	// 	name:req.body.name,
+	// 	phone:req.body.phone
+	// });
+	Contactlist.push(req.body);
+	  return res.redirect('back');
+
+
+	// for checking in the terminal we have returned console.log
+	// console.log(req.body); 
+	// console.log(req.body.name);
+	// console.log(req.body.phone);
+	//return res.redirect('/home');
+    });
     
 
 
 
-// app.listen(port,function(err){
-// 	if(err){
-// 		console.log('error in running server',err);
-// 	}else
-// 	 console.log('yup! my server is running');
-// });
-const express = require('express');
-const path = require('path');
-const port = 10000;
-
-const app = express();
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-
-var contactList = [
-    {
-        name: "Arpan",
-        phone: "1111111111"
-    },
-    {
-        name: "Tony Stark",
-        phone: "1234567890"
-    },
-    {
-        name: "Coding Ninjas",
-        phone: "12131321321"
-    }
-]
-
-app.get('/practice', function(req, res){
-    return res.render('practice', {
-        title: "Let us play with ejs",
-	
-    });
+app.listen(port,function(err){
+	if(err){
+		console.log('error in running server',err);
+	}else
+	 console.log('yup! my server is running');
 });
-
-
-app.get('/', function(req, res){
-    return res.render('home',{
-        title: "Contact List",
-	Contact_list: contactList
-    });
-})
-app.post('/create-contact', function(req, res){
-	return res.redirect('/practice');
-    });
-
-app.listen(port, function(err){
-    if (err) {
-        console.log("Error in running the server", err);
-    }
-    console.log('Yup!My Server is running on Port', port);
-})
